90abab2e4246757ddcbb0780aefae94ab11cd50a
abayeneni <37790232+abayeneni@users.noreply.github.com>
2018-04-17 23:34:48 -0400

    Update capacity-feed.component.ts

--- src/app/capacity-feed/capacity-feed.component.ts
+++ src/app/capacity-feed/capacity-feed.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapacityFeedComponent implements OnInit {
 
+public myFile:any;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+changeListener($event) : void {
+    this.readThis($event.target);
+  }
+
+  readThis(inputValue: any) : void {
+    var file:File = inputValue.files[0]; 
+    var myReader:FileReader = new FileReader();
+
+    myReader.onloadend = function(e){
+      // you can perform an action with readed data here
+      console.log(" output :"+ myReader.result);
+    }
+
+    myReader.readAsText(file);
+  }
+
+
 }
+
